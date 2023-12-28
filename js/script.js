ea3bc7671fdddb1ef04a45cd99e5d5f1245e4389
IgorJF <111748228+IgorJF@users.noreply.github.com>
2023-12-28 19:16:02 -0300

    Adição dos Ultimos Tripulantes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function hideAllPirates() {
     const pirates = document.querySelectorAll('.pirata');
     pirates.forEach(pirate => pirate.classList.remove('show'));
-    document.querySelectorAll('.overlay-image, .effects-image, .green-image, .florest-image').forEach(element => {
+    document.querySelectorAll('.overlay-image, .effects-image, .green-image, .florest-image, .water-image').forEach(element => {
         element.style.display = 'none';
     });
 }
@@ -45,33 +45,33 @@ document.getElementById('jrchopper').addEventListener('click', function() {
     hideAllPirates();
     document.getElementById('imgChopper').classList.add('show');
     document.getElementById('overlayPink').style.display = 'block';
-    document.getElementById('florestEffect').style.display = 'block';
+    document.getElementById('petalasEffect').style.display = 'block';
 });
 
 document.getElementById('jrrobin').addEventListener('click', function() {
     hideAllPirates();
     document.getElementById('imgRobin').classList.add('show');
     document.getElementById('overlayPurple').style.display = 'block';
-    document.getElementById('florestEffect').style.display = 'block';
+    document.getElementById('purpleEffect').style.display = 'block';
 });
 
 document.getElementById('jrfranky').addEventListener('click', function() {
     hideAllPirates();
     document.getElementById('imgFranky').classList.add('show');
     document.getElementById('overlayBlue').style.display = 'block';
-    document.getElementById('florestEffect').style.display = 'block';
+    document.getElementById('blueEffect').style.display = 'block';
 });
 
 document.getElementById('jrbrook').addEventListener('click', function() {
     hideAllPirates();
     document.getElementById('imgBrook').classList.add('show');
     document.getElementById('overlayWhite').style.display = 'block';
-    document.getElementById('florestEffect').style.display = 'block';
+    document.getElementById('notasEffect').style.display = 'block';
 });
 
 document.getElementById('jrjinbei').addEventListener('click', function() {
     hideAllPirates();
     document.getElementById('imgJinbei').classList.add('show');
     document.getElementById('overlayDarkBlue').style.display = 'block';
-    document.getElementById('florestEffect').style.display = 'block';
+    document.getElementById('ondasEffect').style.display = 'block';
 });
