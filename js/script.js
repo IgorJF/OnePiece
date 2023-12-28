8a421f4ef20d7992c94bd8534c79c540d100c56b
IgorJF <111748228+IgorJF@users.noreply.github.com>
2023-12-28 17:42:02 -0300

    Adição de Efeito de Transição entre os Piratas

--- js/script.js
+++ js/script.js
@@ -1,40 +1,28 @@
 function hideAllPirates() {
-    document.getElementById('imgLuffy').style.display = 'none';
-    document.getElementById('imgZoro').style.display = 'none';
-    document.getElementById('imgNami').style.display = 'none';
-    document.getElementById('overlayRed').style.display = 'none';
-    document.getElementById('overlayGreen').style.display = 'none';
-    document.getElementById('overlayOrange').style.display = 'none';
-    document.getElementById('greenSmoke').style.display = 'none';
-    document.getElementById('redSmoke').style.display = 'none';
-    document.getElementById('yellowEffect').style.display = 'none';
+    const pirates = document.querySelectorAll('.pirata');
+    pirates.forEach(pirate => pirate.classList.remove('show'));
+    document.querySelectorAll('.overlay-image, .effects-image, .green-image').forEach(element => {
+        element.style.display = 'none';
+    });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    hideAllPirates();
-});
-
-// ...seu código JavaScript existente...
 
 document.getElementById('jrluffy').addEventListener('click', function() {
     hideAllPirates();
-    document.getElementById('imgLuffy').style.display = 'block';
-    document.getElementById('overlayRed').style.display = 'block'; 
+    document.getElementById('imgLuffy').classList.add('show');
+    document.getElementById('overlayRed').style.display = 'block';
     document.getElementById('redSmoke').style.display = 'block';
 });
 
 document.getElementById('jrzoro').addEventListener('click', function() {
     hideAllPirates();
-    document.getElementById('imgZoro').style.display = 'block';
-    document.getElementById('overlayGreen').style.display = 'block'; 
+    document.getElementById('imgZoro').classList.add('show');
+    document.getElementById('overlayGreen').style.display = 'block';
     document.getElementById('greenSmoke').style.display = 'block';
 });
 
 document.getElementById('jrnami').addEventListener('click', function() {
     hideAllPirates();
-    document.getElementById('imgNami').style.display = 'block';
-    document.getElementById('overlayOrange').style.display = 'block'; 
+    document.getElementById('imgNami').classList.add('show');
+    document.getElementById('overlayOrange').style.display = 'block';
     document.getElementById('yellowEffect').style.display = 'block';
 });
-
-
