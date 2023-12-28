880babb2b72bfe99c385054cfdc86011d8b1c7ac
IgorJF <111748228+IgorJF@users.noreply.github.com>
2023-12-28 17:22:23 -0300

    Adição de Imagens de Efeito

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ function hideAllPirates() {
     document.getElementById('overlayRed').style.display = 'none';
     document.getElementById('overlayGreen').style.display = 'none';
     document.getElementById('overlayOrange').style.display = 'none';
+    document.getElementById('greenSmoke').style.display = 'none';
+    document.getElementById('redSmoke').style.display = 'none';
+    document.getElementById('yellowEffect').style.display = 'none';
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -16,19 +19,22 @@ document.addEventListener('DOMContentLoaded', function() {
 document.getElementById('jrluffy').addEventListener('click', function() {
     hideAllPirates();
     document.getElementById('imgLuffy').style.display = 'block';
-    document.getElementById('overlayRed').style.display = 'block'; // Exibe a imagem de sobreposição vermelha
+    document.getElementById('overlayRed').style.display = 'block'; 
+    document.getElementById('redSmoke').style.display = 'block';
 });
 
 document.getElementById('jrzoro').addEventListener('click', function() {
     hideAllPirates();
     document.getElementById('imgZoro').style.display = 'block';
-    document.getElementById('overlayGreen').style.display = 'block'; // Exibe a imagem de sobreposição verde
+    document.getElementById('overlayGreen').style.display = 'block'; 
+    document.getElementById('greenSmoke').style.display = 'block';
 });
 
 document.getElementById('jrnami').addEventListener('click', function() {
     hideAllPirates();
     document.getElementById('imgNami').style.display = 'block';
-    document.getElementById('overlayOrange').style.display = 'block'; // Exibe a imagem de sobreposição laranja
+    document.getElementById('overlayOrange').style.display = 'block'; 
+    document.getElementById('yellowEffect').style.display = 'block';
 });
 
 
